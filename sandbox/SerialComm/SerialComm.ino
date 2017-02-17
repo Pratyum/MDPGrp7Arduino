9290fdf866079155bb87cf03e74c58e1fc6ba086
jeffreyong92 <JONG044@e.ntu.edu.sg>
2017-02-17 15:44:45 +0800

    SerialComm (with RPi)

--- sandbox/SerialComm/SerialComm.ino
+++ sandbox/SerialComm/SerialComm.ino
@@ -1,22 +1,13 @@
-
-String tmp = "";
-
 void setup() {
   Serial.begin(9600);
 }
 
 void loop()
 {
+  String tmp = "";
   while(Serial.available() == 0);
   Serial.setTimeout(10);
   tmp = Serial.readString();
 
   Serial.println(tmp);
-
-//  if (tmp == "ABC") {
-//    Serial.println("ABC");
-//  }
-//  else {
-//    Serial.println("Others");
-//  }
 }
